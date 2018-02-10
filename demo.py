c5fb9f2c4bf1ec6ea2ae4d526dcaeb7caa6cceca
wangyanfang <yaobo324413@163.com>
2018-02-10 10:16:04 +0800

    append GPL

--- demo.py
+++ demo.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python    #这句会去 环境设置env 中寻找python路径
 # -*- coding:utf-8 -*-
 
-# #demo1
-# #定义初始值
-# start=1;
-# while True:
-# #判断start的值若其为51，则说明上一次已经输出了100，跳出循环
-#    if start==51:
-#      break
-#    print (start*2);
-#    start +=1;
+#demo1
+#定义初始值
+start=1;
+while True:
+#判断start的值若其为51，则说明上一次已经输出了100，跳出循环
+   if start==51:
+     break
+   print (start*2);
+   start +=1;
 
 # #demo2
 # import sys; x= 'runoob'; sys.stdout.write(x + '\n');
