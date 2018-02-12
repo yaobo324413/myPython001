73e03228fcea808b2801a31dbb2d6fac6d21631b
wangyanfang <yaobo324413@163.com>
2018-02-12 15:06:28 +0800

    9

--- test01/demo3.py
+++ test01/demo3.py
@@ -132,32 +132,6 @@ print (x,y)
 #     import sys;
 #     sys.stdout.write(binum.pop());  #无空格输出print'(2)'
 
-# #demo12：九九乘法表
-# i = 1;
-# while i:
-#     j = 1;
-#     while j:
-#         print (j,"*",i,"=",i*j,''),
-#         if i==j:
-#             break;
-#
-#         j +=1;
-#         if j >= 10:
-#             break;
-#     print("\n");
-#     i += 1;
-#     if i >= 10:
-#         break;
-
-#demo13：输出 2 到 100 简的质数
-prime = [];
-for num in range(2,100):  # 迭代 2 到 100 之间的数字
-   for i in range(2,num): # 根据因子迭代
-      if num%i == 0:      # 确定第一个因子
-         break;           # 跳出当前循环
-   else:                  # 循环的 else 部分
-      prime.append(num)
-print (prime);
 
 
 
